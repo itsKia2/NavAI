eeb4adb4284ea9d692fc2d6b56fd8b3d0dd961d9
itsKia2 <omerkarimi29@gmail.com>
2024-10-31 18:05:59 -0400

    replaced openai interface with langchain; TODO switch to semantic chunking

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
-import { genSaveEmbeds } from "./backend/supa.js";
-import { runQuery } from "./backend/langchain.js";
 import path from "path";
 import { createClient } from "@supabase/supabase-js";
-import OpenAI from "openai";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+
+import { savePdfEmbed } from "./backend/supa.js";
+import { runQuery } from "./backend/langchain.js";
 
 /* INITIALIZATION */
 
 function loadingEnv() {
-	// Load environment variables (if using dotenv)
+	// Load environment variables using dotenv
 	const SUPABASE_URL = process.env.SUPABASE_URL;
 	const SUPABASE_API_KEY = process.env.SUPABASE_KEY;
 	const openaikey = process.env.OPENAI_API_KEY;
 
-	const config = new OpenAI({ apiKey: openaikey });
-	const openai = new OpenAI(config);
-	const client = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+	// Chat client
+	const openai = new ChatOpenAI({
+		apiKey: openaikey,
+		model: "gpt-3.5-turbo",
+		temperature: 0.3,
+	});
+
+	// Embedding client
+	const embeddings = new OpenAIEmbeddings({
+		apiKey: openaikey,
+		model: "text-embedding-ada-002",
+	});
+
+	// Supabase client
+	const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
 	// Create the Supabase client and OpenAI key
 	return {
-		supa: client,
-		openai: openai,
+		supa: supabase,
+		chat: openai,
+		embeddings: embeddings,
 	};
 }
 
 // Used to load Supabase client and OpenAI API
-const { supa, openai } = loadingEnv();
+const { supa, chat, embeddings } = loadingEnv();
+console.log("Env variables loaded");
 let filepath = path.resolve("./assets/op4_rto_final.pdf");
 // Name of table in Supabase
 const tableName = "pdfEmbedding";
 
 /* EXECUTION */
-
 const query =
 	"What is the Supervisor of Short Term Outage Coordination responsible for during Operating Procedure No. 3 - Transmission Outage Scheduling (OP-3) event";
 
 // Create query using existing vectors present in Supabase
-runQuery(query, openai, supa);
+// runQuery(query, chat, embeddings, supa);
+// savePdfEmbed(supa, chat, embeddings, filepath);
