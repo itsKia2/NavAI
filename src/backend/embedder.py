6b7ed287ca9483a39192f770a741be6c63cb0e82
itsKia2 <omerkarimi29@gmail.com>
2024-11-20 14:13:54 -0500

    fixed openai prompt

--- src/backend/embedder.py
+++
@@ -1,9 +0,0 @@
-import openai
-import os
-
-# Set OpenAI API key from .env
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-def generate_embedding(text):
-    response = openai.Embedding.create(input=text, model="text-embedding-ada-002")
-    return response['data'][0]['embedding']
